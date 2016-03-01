04bba41fba334f6d511cb3aba4430d31bd40d12a
Mako <mnfeconicu41@gmail.com>
2016-03-01 19:10:34 +0900

    create jslib command to install js library

---
+++ bash/jslibs/cmd/help.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function jslib-help {
+  echo "Overview:"
+  echo "  JavaScript library install tool"
+  echo
+  echo "Example usage:"
+  echo "  jslib install <library...>"
+  echo
+}
