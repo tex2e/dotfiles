bc0ca1ce2ce417c46a4e04e05a993ab3724fa8e7
Mako <mnfeconicu41@gmail.com>
2016-03-16 19:35:56 +0900

    jslib install [--here] <lib> to install <lib> here

--- bash/jslibs/cmd/install.sh
+++ bash/jslibs/cmd/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# convert a yaml string to a tsv string
 function parse-yaml {
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
@@ -10,12 +11,13 @@ function parse-yaml {
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s %s\n", vn, $2, $3);
+         printf("%s%s\t%s\n", vn, $2, $3);
       }
    }'
    return 0
 }
 
+# download a file from specified url.
 function download {
   if which curl &>/dev/null; then
     curl -O "$@"
@@ -24,14 +26,31 @@ function download {
   fi
 }
 
-# jslib install <library...>
+# jslib install [--download-here] <library...>
 function jslib-install {
-  jslibs=$(parse-yaml $JSLIB_SETTINGS_YAML)
-  echo "$jslibs"
+  # get option
+  local DOWNLOAD_HERE=
+  if [[ "$1" = '--here' ]] || [[ "$1" = '-h' ]]; then
+    DOWNLOAD_HERE=1  # set true
+  fi
+
+  # get setting file
+  local jslibs=$(parse-yaml $JSLIB_SETTINGS_YAML)
+
+  # install libraries
   for library in $@; do
-    install_url=$(echo "$jslibs" | grep -e '\blib_'"$library"'\b' | cut -d ' ' -f 2)
-    test "$install_url" == "" && continue
-    mkdir -p js/lib/ && cd js/lib &&
+    # if library is not jot downed to the setting file, skip it.
+    install_url=$(echo "$jslibs" | grep -e '\blib_'"$library"'\b' | cut -f 2)
+    test "$install_url" = "" && continue
+
+    # if DOWNLOAD_HERE is false
+    if [[ DOWNLOAD_HERE = 0 ]]; then
+      # default path is ./js/lib/lib_name.js
+      mkdir -p js/lib/ && cd js/lib
+    fi
+
+    local install_filename=$(basename "$install_url")
+    echo "install to $PWD/$install_filename"
     download "$install_url"
     cd - &>/dev/null
   done
