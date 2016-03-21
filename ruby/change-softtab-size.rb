2b612fbf38d7c667829b57bb455edec07ebb5925
Mako <mnfeconicu41@gmail.com>
2016-03-21 19:52:57 +0900

    create change-softtab-size.rb

---
+++ ruby/change-softtab-size.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+# chtabsize -- change softtab size
+#
+# SYNOPSIS
+#
+#   chtabsize <before> <after> <filename>
+#
+
+# convert softtab width
+#
+# parameters
+# * before - {int} actual softtab size
+# * after  - {int} expected softtab size
+# * line   - {string} each line
+#
+def convert_softtab_width(before, after, line)
+  before_softtab = " " * before
+  after_softtab  = " " * after
+
+  match0 = line.match(/^(#{before_softtab})*/).to_a[0]
+  indent_depth = match0.length / before
+
+  if indent_depth > 0
+    line.sub!(/^(#{before_softtab})+/, after_softtab * indent_depth)
+  end
+
+  return line
+end
+
+# --- main process ---
+
+unless ARGV.length >= 3
+  raise ArgumentError.new("wrong number of arguments (given #{ARGV.length}, expected 3..")
+end
+
+actual   = ARGV.shift.to_i  # actual_softtab_size
+expected = ARGV.shift.to_i  # expected_softtab_size
+
+ARGV.each do |arg|
+  unless File.exist?(arg)
+    puts "No such file or directory: #{arg}"
+    break
+  end
+
+  lines = []
+
+  # convert softtab width
+  File.open(arg, 'r') do |f|
+    f.each do |line|
+      line = convert_softtab_width(actual, expected, line)
+      lines.push(line)
+    end
+  end
+
+  # write result to origin file
+  File.open(arg, 'w') do |f|
+    f.write(lines.join)
+  end
+end
