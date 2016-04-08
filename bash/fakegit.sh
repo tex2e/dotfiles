f6d1944c0fa90692d4c8c28987fa9c0f9868cd4b
Mako <mnfeconicu41@gmail.com>
2016-04-08 19:06:29 +0900

    add `fakegit` command

---
+++ bash/fakegit.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/env bash
+
+# The MIT License
+#
+# Copyright (c) 2012 Yoshio HANAWA
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# fakegit(1) -- Emulating "git clone" with other tools
+#
+# SYNOPSIS
+#
+#   fakegit clone [-b <branch_name>] <GitHub Repository URL> [<directory>]
+#
+# DESCRIPTION
+#
+#   The fakegit command provides psuedo "git clone" command only for GitHub repository, which downloads files with svn, curl, or wget. This is useful for environments which is difficult to installing git command.
+#
+#   To install fakegit, type as follows:
+#
+#       $ mkdir -p $HOME/bin
+#       $ export PATH=$HOME/bin:$PATH
+#       $ curl -L https://raw.github.com/hnw/fakegit/master/bin/fakegit > $HOME/bin/git
+#       $ chmod a+x $HOME/bin/git
+#       $ hash -r
+#
+#   If necessary, enable fakegit in your shell by adding $HOME/bin to your PATH and restart your shell.
+#
+
+# Instant Usage
+#
+#       $ sh <(curl -L https://raw.github.com/TeX2e/dotfiles/bash/fakegit.sh) clone <URL>
+#
+
+version="1.0.0"
+
+# -e : Exit immediately if a simple command exits with a non-zero status.
+set -e
+
+validate_url() {
+  url="$1"
+  # supports following URLs
+  #  HTTP: https://github.com/*/*.git
+  #  SSH:  git@github.com:*/*.git
+  #  Git:  git://github.com/*/*.git
+  valid_url=$(echo "$1" | grep '^\(https://github.com/\|git@github.com:\|git://github.com/\)[^/]*/[^/]*.git$' || true)
+  if [ -z "$valid_url" ]; then
+    echo "fakegit: Specified invalid GitHub URL \`$url'" >&2
+    exit 1
+  fi
+}
+
+fetch_command() {
+  if type curl &>/dev/null; then
+    echo "curl" "-L" "$@"
+  elif type wget &>/dev/null; then
+    echo "wget" "-O-" "$@"
+  fi
+}
+
+log() {
+  printf "fakegit: %b\n" "$*" >&2
+  return $?
+}
+
+fail() {
+  log "ERROR: $*\n"
+  exit 1
+}
+
+fakegit_clone() {
+  local branch_name="master"
+
+  if [ "$1" = "-b" ] || [ "$1" = "--branch" ]; then
+    branch_name="$2"
+    if [ -z "$branch_name" ] ; then
+      fail "switch \`$1' requires a value"
+      exit 1
+    fi
+    shift 2
+  fi
+
+  local git_repo="$1"
+
+  if [ -z "$git_repo" ]; then
+    echo "usage: $0 clone [-b <branch_name>] <GitHub Repository URL> [<directory>]" >&2
+    exit 1
+  fi
+
+  validate_url $git_repo
+  # Git URL -> HTTP URL
+  local https_repo="${git_repo/#git:/https:}"
+  # SSH URL -> HTTP URL
+  https_repo="${https_repo/#git@github.com:/https://github.com/}"
+  local svn_repo="${https_repo%*.git}"
+  local proj_name="${svn_repo##*/}"
+  local dir="${2:-$proj_name}"
+
+  if type svn &>/dev/null; then
+    local svn_dir="trunk"
+    if [ "$branch_name" != "master" ] ; then
+      svn_dir="branches/${branch_name}"
+    fi
+    local svn_command="svn checkout ${svn_repo}/${svn_dir} $dir"
+    log "Instaed of git, executing:\n" "$svn_command\n"
+    echo $svn_command
+    printf "\n"
+    log "See details for GitHub Subversion support:\n" "https://github.com/blog/966-improved-subversion-client-support"
+  else
+    local fetch_command=$(fetch_command "${svn_repo}/tarball/${branch_name}" || true)
+    local extract_command="tar xzf - --strip-components 1 -C $dir"
+    if [ -z "$fetch_command" ] ; then
+      fail "No download command found.\nPlease install one of \`svn', \`curl' or \`wget' and try again"
+
+      exit 1
+    fi
+    mkdir -p $dir
+    log "Instaed of git, executing:\n" $fetch_command "|" $extract_command
+    $fetch_command | $extract_command
+  fi
+}
+
+fakegit_help() {
+  case "$1" in
+  "" )
+    echo "usage: fakegit <command> [<args>]"
+    ;;
+  "clone" )
+    echo "usage: fakegit clone <GitHub repository> [<directory>]"
+    ;;
+  * )
+    command_path="$(command -v "fakegit_$1" || true)"
+    if [ -n "$command_path" ]; then
+      echo "Sorry, the \`$1' command isn't documented yet."
+      echo
+      echo "You can view the command's source here:"
+      echo "$command_path"
+      echo
+    else
+      echo "fakegit: no such command \`$1'"
+    fi
+  esac
+}
+
+command="$1"
+case "$command" in
+  "" | "-h" | "--help" )
+    echo -e "fakegit ${version}\n$(fakegit_help)" >&2
+    ;;
+  * )
+    command_path="$(command -v "fakegit_$command" || true)"
+    if [ -z "$command_path" ]; then
+      echo "fakegit: Unsupported command \`$command'" >&2
+      exit 1
+    fi
+
+    shift 1
+    "$command_path" "$@"
+  ;;
+esac
