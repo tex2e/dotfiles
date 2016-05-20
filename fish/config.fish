d0c4c4029110fcaa1e0773c291aed6401fc247b7
Mako <mnfeconicu41@gmail.com>
2016-05-20 21:06:57 +0900

    fish: add useful functions

--- fish/config.fish
+++ fish/config.fish
@@ -52,3 +52,73 @@ function fish_right_prompt -d "Write out the right prompt"
   printf '%s%s%s ' \
     (set_color $color) $branch (set_color normal)
 end
+
+
+### Useful Functions ###
+
+function mkdircd
+  command mkdir -p "$argv[1]"; and cd "$argv[1]"
+end
+
+function path
+  set -l IFS ':'
+  for p in $PATH
+    echo $p
+  end
+end
+
+function cdpath
+  set -l IFS ':'
+  for p in $CDPATH
+    echo $p
+  end
+end
+
+function fpath
+  set -l IFS ':'
+  for p in $FPATH
+    echo $p
+  end
+end
+
+function manpath
+  set -l IFS ':'
+  set -l MPATH (command manpath)
+  for p in $MPATH
+    echo $p
+  end
+end
+
+function extract
+  if not test -f "$argv[1]"
+    echo "'$1' is not a valid file"
+    return
+  end
+
+  switch "$argv[1]"
+  case '*.tar.bz2'
+    tar xjf "$argv[1]"
+  case '*.tar.gz'
+    tar xzf "$argv[1]"
+  case '*.bz2'
+    bunzip2 "$argv[1]"
+  case '*.rar'
+    unrar e "$argv[1]"
+  case '*.gz'
+    gunzip "$argv[1]"
+  case '*.tar'
+    tar xf "$argv[1]"
+  case '*.tbz2'
+    tar xjf "$argv[1]"
+  case '*.tgz'
+    tar xzf "$argv[1]"
+  case '*.zip'
+    unzip "$argv[1]"
+  case '*.Z'
+    uncompress "$argv[1]"
+  case '*.7z'
+    7z x "$argv[1]"
+  case '*'
+    echo "'$1' cannot be extracted via extract()"
+  end
+end
