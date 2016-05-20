96e1049841ea814d6636a95d55c7bc3925839e59
Mako <mnfeconicu41@gmail.com>
2016-05-20 19:40:23 +0900

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 
 function fish_prompt -d "Write out the prompt"
-  set color $fish_color_cwd
+  set -l color $fish_color_cwd
   if test $status -ne 0
     set color $fish_color_error
   end
@@ -10,6 +10,20 @@ function fish_prompt -d "Write out the prompt"
 end
 
 function fish_right_prompt -d "Write out the right prompt"
-  printf '%s' \
-    (__fish_git_prompt)
+  test -d "$PWD/.git"; or return
+
+  set st (git status 2> /dev/null)
+  if echo "$st" | grep "nothing to"
+    set color green
+  else if echo "$st" | grep "nothing added"
+    set color yellow
+  else if echo "$st" | grep "Untracked files"
+    set color red --bold
+  else
+    set color red
+  end
+
+  printf '%s%s%s%s' \
+    (set_color $color) (__fish_git_prompt) (set_color normal) \
+    (__fish_git_prompt_informative_status)
 end
