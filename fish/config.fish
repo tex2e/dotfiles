5abc2f2305e627f71a9924aac05fd9f30d5b794f
Mako <mnfeconicu41@gmail.com>
2016-05-20 23:03:50 +0900

    override fish_title

--- fish/config.fish
+++ fish/config.fish
@@ -72,6 +72,37 @@ function fish_right_prompt -d "Write out the right prompt"
     (set_color $color) $branch (set_color normal)
 end
 
+# Change the terminal title automatically based on current process / working-directory
+#
+# The main improvement over the default 'fish_title' behavior
+# is that I use the name of the current git repo, if any, as
+# opposed to the raw working-directory
+function fish_title
+  set -l command (echo $_)
+  if not test $command = "fish"
+    # we are busy running some non-fish command, so use the command name
+    echo $command
+    return
+  end
+  # we are sitting at the fish prompt
+
+  if not git rev-parse --git-dir > /dev/null ^ /dev/null
+    # we are NOT inside a git repo, so just use the working-directory
+    echo (pwd)
+    return
+  end
+  # we are inside a git directory, so use the name of the repo as the terminal title
+
+  set -l git_dir (git rev-parse --git-dir)
+  if test $git_dir = ".git"
+    # we are at the root of the git repo
+    echo (basename (pwd))
+  else
+    # we are at least one level deep in the git repo
+    echo (basename (dirname $git_dir))
+  end
+end
+
 
 ### Useful Functions ###
 
