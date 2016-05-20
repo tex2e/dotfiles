12f08839eb87497d99b06bc38e5713cc84fe487d
Mako <mnfeconicu41@gmail.com>
2016-05-20 19:51:57 +0900

    fish: update fish_right_prompt

--- fish/config.fish
+++ fish/config.fish
@@ -9,21 +9,31 @@ function fish_prompt -d "Write out the prompt"
     (prompt_pwd) (set_color $color) (set_color normal)
 end
 
+# load __fish_git_prompt
+__fish_git_prompt > /dev/null
+
 function fish_right_prompt -d "Write out the right prompt"
   test -d "$PWD/.git"; or return
 
+  # branch name
+  set -l head (git symbolic-ref HEAD 2> /dev/null)
+  test -z $head; and return
+  set -l branch (basename $head)
+  test -z $branch; and return
+
+  # set color
   set st (git status 2> /dev/null)
-  if echo "$st" | grep "nothing to"
+  if echo "$st" | grep "nothing to" > /dev/null
     set color green
-  else if echo "$st" | grep "nothing added"
+  else if echo "$st" | grep "nothing added" > /dev/null
     set color yellow
-  else if echo "$st" | grep "Untracked files"
+  else if echo "$st" | grep "Untracked files" > /dev/null
     set color red --bold
   else
     set color red
   end
 
-  printf '%s%s%s%s' \
-    (set_color $color) (__fish_git_prompt) (set_color normal) \
+  printf '%s%s%s %s' \
+    (set_color $color) (echo $branch) (set_color normal) \
     (__fish_git_prompt_informative_status)
 end
