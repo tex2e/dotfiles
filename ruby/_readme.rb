86372f864c1bc963b4cc2eb1ea753989d49024ad
Mako <mnfeconicu41@gmail.com>
2016-05-22 17:46:59 +0900

    Makefile: create ruby scripts doc

---
+++ ruby/_readme.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#
+# This file is meta description for documentation.
+#
+
+#:readme:
+# Scripts
+# ==========
+#
+# To use scripts, export PATH:
+#
+#     export PATH="$PATH:$HOME/.dotfiles/bin"'
+#
