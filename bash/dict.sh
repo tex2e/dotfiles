6e02d112a058a8de3c0a0e80ee26a4411f871cd4
Mako <mnfeconicu41@gmail.com>
2016-06-18 10:40:32 +0900

    update document

--- bash/dict.sh
+++ bash/dict.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+#:readme:
+#
+# ## dict(1) -- a command line dictionary for UNIX
+#
+# [code](dict.sh)
+#
+# ### SYNOPSIS
+#
+#     dict <word>
+#
+# ### Description
+#
+# a command line dictionary for UNIX
+#
+# ### Usage
+#
+#     dict rudimentary
+#     dict laconic
+#
 
 curl "dict://dict.org/d:$1:*" | less
