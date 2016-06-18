715e60fe5e9d4cb5807cac64f7d9ee66a70f4b9b
Mako <mnfeconicu41@gmail.com>
2016-06-18 22:53:10 +0900

    add gdiff()

--- python/pyzact/test/conv_test.sh
+++ python/pyzact/test/conv_test.sh
@@ -2,6 +2,34 @@
 
 SCRIPT="./conv"
 
+function gdiff {
+  local REG=`tput op`
+  local GRP=`tput setaf 6`
+  local ADD=`tput setaf 2`
+  local REM=`tput setaf 1`
+
+  local NL=$'\n'
+  local GRP_LABEL="${GRP}@@ %df,%dn +%dF,%dN @@${REG}"
+
+  local UNCH_GRP_FMT=''
+
+  if [[ "${1}" == '@full' ]]; then
+    UNCH_GRP_FMT="${GRP_LABEL}${NL}%="
+    shift
+  fi
+
+  diff \
+    --new-line-format="${ADD}+%L${REG}" \
+    --old-line-format="${REM}-%L${REG}" \
+    --unchanged-line-format=" %L${REG}" \
+    --new-group-format="${GRP_LABEL}${NL}%>" \
+    --old-group-format="${GRP_LABEL}${NL}%<" \
+    --changed-group-format="${GRP_LABEL}${NL}%<%>" \
+    --unchanged-group-format="${UNCH_GRP_FMT}" \
+      "${@}" | less -FXR
+}
+
+
 cmd="seq 10 | $SCRIPT 2"
 expect="\
 1 2
@@ -16,7 +44,7 @@ expect="\
 actual=$(eval $cmd)
 
 echo "> $cmd"
-diff -u <(echo "$expect") <(echo "$actual")
+gdiff <(echo "$expect") <(echo "$actual")
 
 
 cmd="yes | awk '\$0=NR' | $SCRIPT 3 | head -5"
@@ -25,9 +53,8 @@ expect="\
 2 3 4
 3 4 5
 4 5 6
-aaaaa
 5 6 7"
 actual=$(eval $cmd)
 
 echo "> $cmd"
-diff -u <(echo "$expect") <(echo "$actual")
+gdiff <(echo "$expect") <(echo "$actual")
