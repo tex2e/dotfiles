9f69532142b21e694e21e9d8472af0ef1a35da98
Mako <mnfeconicu41@gmail.com>
2016-07-22 12:26:34 +0900

    Update git-open.sh

--- bash/git-open.sh
+++ bash/git-open.sh
@@ -7,17 +7,22 @@ function usage {
   echo "Usage: git-open [<remote>]"
 }
 
-which open     &>/dev/null && OPEN=open
-which xdg-open &>/dev/null && OPEN=xdg-open
-
 case $1 in
   -h | --help)
     usage; exit 1;;
 esac
 
+# set open command
+which open     &>/dev/null && OPEN=open
+which xdg-open &>/dev/null && OPEN=xdg-open
+if [[ $OPEN == "" ]]; then
+  echo "You must install `open` command before running this script."
+  exit 1
+fi
+
+# set URL
 REMOTE=${1:-origin}
 URL=$(git remote -v | grep "$REMOTE" | awk '{print $2}' | uniq)
-
 if [[ $URL == git@* ]]; then
   # convert SSH to HTTPS
   URL=$(
@@ -32,15 +37,12 @@ if [[ $URL == "" ]]; then
   exit 1
 fi
 
-# --- open a site ---
+# --- open site --
 
-case $URL in
-  https://*)
-    : ;;
-  *)
-    echo "URL \"$URL\" must be started with \"https://\""
-    exit 1;;
-esac
+if [[ $URL != https://* ]]; then
+  echo "URL \"$URL\" must starts with \"https://\""
+  exit 1;;
+fi
 
 echo "> $OPEN $URL"
 $OPEN "$URL" &>/dev/null
