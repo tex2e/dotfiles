aef8f7eadc802c13d20d5c685470a4b70fefb89a
Mako <mnfeconicu41@gmail.com>
2016-07-22 10:01:48 +0900

    git-open: more user frendly

--- bash/git-open.sh
+++ bash/git-open.sh
@@ -3,10 +3,15 @@
 # git-open -- open repository's origin site
 #
 
+function usage {
+  echo "Usage: git-open [<remote>]"
+}
+
 which open     &>/dev/null && OPEN=open
 which xdg-open &>/dev/null && OPEN=xdg-open
 
-URL=$(git remote -v | grep origin | awk '{print $2}' | uniq)
+REMOTE=${1:-origin}
+URL=$(git remote -v | grep "$REMOTE" | awk '{print $2}' | uniq)
 
 if [[ $URL == git@* ]]; then
   # convert SSH to HTTPS
@@ -16,4 +21,18 @@ if [[ $URL == git@* ]]; then
     sed 's,git@,https://,')
 fi
 
-$OPEN $URL
+if [[ $URL == "" ]]; then
+  echo "Remote \"$REMOTE\" not found."
+  usage
+  exit 1
+fi
+
+# --- open a site ---
+
+echo "> $OPEN $URL"
+$OPEN "$URL" &>/dev/null
+
+if [[ $? -ne 0 ]]; then
+  echo "URL \"$URL\" not found."
+  exit 1
+fi
