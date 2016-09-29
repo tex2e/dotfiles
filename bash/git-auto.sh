448d09b6b66e4d9508e5a697558cc28e1cd144ae
Mako <mnfeconicu41@gmail.com>
2016-09-29 22:13:54 +0900

    [create] bash/git-auto.sh, bin/git-auto

---
+++ bash/git-auto.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# git-auto -- stage && commit && push
+#
+
+function usage {
+  echo "Usage: git-auto [-h|--help]"
+}
+
+case $1 in
+  -h | --help)
+    usage; exit 1;;
+esac
+
+
+function is_inside_repo {
+  if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+    echo "pwd is not inside the git repo." >&2
+    return 1
+  fi
+}
+
+function gen_commit_message {
+  local NEWLINE="
+"
+
+  cd $(git rev-parse --show-toplevel)
+
+  short_status=$(git status -s)
+  A_file=$(echo "$short_status" | grep '^A' | cut -c 4-)
+  M_file=$(echo "$short_status" | grep '^M' | cut -c 4-)
+  R_file=$(echo "$short_status" | grep '^R' | cut -c 4-)
+  D_file=$(echo "$short_status" | grep '^D' | cut -c 4-)
+
+  message=""
+
+  if [[ -n $A_file ]]; then
+    message+="[create] ${A_file//$NEWLINE/, } $NEWLINE"
+  fi
+
+  if [[ -n $M_file ]]; then
+    message+="[update] ${M_file//$NEWLINE/, } $NEWLINE"
+  fi
+
+  if [[ -n $R_file ]]; then
+    message+="[rename] ${R_file//$NEWLINE/, } $NEWLINE"
+  fi
+
+  if [[ -n $D_file ]]; then
+    message+="[delete] ${D_file//$NEWLINE/, } $NEWLINE"
+  fi
+
+  echo "$message"
+}
+
+function get_tracking_remote_name {
+  git rev-parse --abbrev-ref --symbolic-full-name @{u} \
+    | sed -e 's,/, ,'
+}
+
+function _sh {
+  echo "> $*"
+  eval "$*"
+}
+
+is_inside_repo || exit 1
+_sh "git add --all" || exit 1
+_sh "git commit -m \"$(gen_commit_message)\"" || exit 1
+_sh "git push $(get_tracking_remote_name)" || exit 1
+exit 0
