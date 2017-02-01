fa38eb43c32fe07dab2cb4d3357229d3ac5f8277
Mako <mnfeconicu41@gmail.com>
2017-02-01 22:28:47 +0900

    add comments

--- bash/sweep.sh
+++ bash/sweep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-find . -name .DS_Store -type f -delete
-xattr -c $(ls -l | grep -e '^..........@' | awk '{print $NF}' )
+# Delete .DS_Store under the cwd.
+# Delete extended attribute keys from file in the cwd.
+
+find . -type f -name .DS_Store -delete
+xattr -c $(ls -l | grep -e '^..........@' | awk '{print $NF}')
