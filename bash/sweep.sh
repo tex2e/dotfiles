5c070e34ed4cb2187477cfe319d55004a91df4cc
Mako <mnfeconicu41@gmail.com>
2017-02-13 23:27:16 +0900

    update some command.sh

--- bash/sweep.sh
+++ bash/sweep.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-
-# Delete .DS_Store
-# Delete extended attribute keys
+#:readme:
+#
+# ## sweep(1) -- Sweep MacOS metadata under cwd (only MacOS)
+#
+# [code](sweep.sh)
+#
+# ### SYNOPSIS
+#
+#     sweep [<path/to/dir>]
+#
+# ### Description
+#
+# Delete .DS_Store and delete extended attribute keys which added to files.
+#
 
 find . -type f -name .DS_Store -delete
 find . -type f -name '*' -exec xattr -c {} +
