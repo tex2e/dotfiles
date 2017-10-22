0731ec29c3b4f5f766d190e3f3bd713a251d8ab2
Mako <mnfeconicu41@gmail.com>
2017-10-22 18:09:29 +0900

    add jpg-compress.sh

---
+++ bash/jpg-compress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#:readme:
+#
+# ## jpg-compress(1) -- Compress jpg files under cwd
+#
+# [code](jpg-compress.sh)
+#
+# ### SYNOPSIS
+#
+#     jpg-compress [<path/to/dir>]
+#
+# ### Description
+#
+# Compress jpg files under current working directory.
+# In advance you need to install "jpegoptim" command.
+#
+
+if ! which jpegoptim &>/dev/null; then
+  echo "$0: command not found: jpegoptim"
+  exit 1
+fi
+
+for jpg in `find "${1:-.}" -iname "*.jpg"`;
+do
+  echo "Compressing $jpg"
+  jpegoptim --max=90 "$jpg"
+done
