4318f196e6fc054a9015b1d51d4ebad055a2e955
Mako <mnfeconicu41@gmail.com>
2020-05-06 15:31:50 +0900

    rm bash-on-windows/*

--- bash-on-windows/open.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-  DIR=.
-else
-  DIR="$*"
-fi
-
-explorer.exe $DIR
--- bash-on-windows/w2u.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# w2u -- Convert path from window to linux
-#
-# Usage:
-#   > echo "C:\path\to\dir" | w2u
-#   /mnt/c/path/to/dir
-
-sed 's,\\,/,g' | sed -r 's,^(.)(:),/mnt/\L\1,g'
