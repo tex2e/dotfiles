6a10b8ed53c1939f2bc857a333a0dd3162c44a82
Mako <mnfeconicu41@gmail.com>
2020-09-05 22:39:16 +0900

    create ahk/setup.cmd

--- ahk/setup.cmd
+++ ahk/setup.cmd
@@ -1,31 +1,6 @@
 @echo off
 
-SET SHELL_STARTUP=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup
-
-REM --- AutoHotkeyScript.ahk ---
-SET AHK_SCRIPT=AutoHotkeyScript.ahk
-SET AHK_EXE=AutoHotkeyScript.exe
-REM 1. Compile
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%AHK_SCRIPT%" /out "%AHK_EXE%"
-REM 2. Kill AutoHotKey process
-TASKKILL /im AutoHotkeyScript.exe
-TIMEOUT 1
-REM 3. Move exe to shell:startup folder
-MOVE /y "%AHK_EXE%" "%SHELL_STARTUP%"
-REM 4. Execute AutoHotKey
-START /b "AutoHotKey" "%SHELL_STARTUP%\%AHK_EXE%"
-
-REM --- HotCorners.ahk ---
-SET AHK_SCRIPT=HotCorners.ahk
-SET AHK_EXE=HotCorners.exe
-REM 1. Compile
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "%AHK_SCRIPT%" /out "%AHK_EXE%"
-REM 2. Kill AutoHotKey process
-TASKKILL /im HotCorners.exe
-TIMEOUT 1
-REM 3. Move exe to shell:startup folder
-MOVE /y "%AHK_EXE%" "%SHELL_STARTUP%"
-REM 4. Execute AutoHotKey
-START /b "AutoHotKey" "%SHELL_STARTUP%\%AHK_EXE%"
+CALL setup-autohotkey.cmd
+CALL setup-hotcorner.cmd
 
 PAUSE
