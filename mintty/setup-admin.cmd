3d800583618ede9adcb1194c84458dcabd3a42b3
Mako <mnfeconicu41@gmail.com>
2021-01-05 21:31:23 +0900

    create mintty settings

---
+++ mintty/setup-admin.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set TO_DIR=%HomeDrive%%HomePath%
+set FROM_DIR=%~dp0
+
+openfiles > NUL 2>&1
+if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
+  mkdir C:\Users\mnfec\.mintty\themes
+  call :backup_and_mklink .mintty\themes\base16-google-light.minttyrc base16-google-light.minttyrc
+  echo [info]: Setup Finished!
+goto End
+:NotAdmin
+  echo This command prompt is NOT ELEVATED
+:End
+
+pause
+
+exit /b
+
+
+REM サブルーチン：バックアップの作成とリンクの作成
+:backup_and_mklink
+if exist "%TO_DIR%\%~1" (
+  MOVE "%TO_DIR%\%~1" "%TO_DIR%\%~1.orig"
+)
+if "%~2"=="" (
+  MKLINK "%TO_DIR%\%~1" "%FROM_DIR%\%~1"
+) else (
+  MKLINK "%TO_DIR%\%~1" "%FROM_DIR%\%~2"
+)
+exit /b
