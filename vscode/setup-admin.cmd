3a3f3118398604b135047ab618ff8bc140300b37
Mako <mnfeconicu41@gmail.com>
2021-04-04 17:07:19 +0900

    update vscode settings

--- vscode/setup-admin.cmd
+++ vscode/setup-admin.cmd
@@ -1,29 +1,41 @@
 @echo off
 
 REM VSCode settings directory
+
+REM リンク先フォルダとリンク元フォルダ
 SET TO_DIR=%AppData%\Code\User
 SET FROM_DIR=%~dp0
 
-IF exist "%TO_DIR%\settings.json" (
-  MOVE "%TO_DIR%\settings.json" "%TO_DIR%\settings.json.orig"
+REM 管理者権限チェック
+openfiles > NUL 2>&1
+if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
+
+  call :backup_and_mklink settings.json
+  call :backup_and_mklink keybindings.json
+  call :backup_and_mklink snippets\markdown.json
+  call :backup_and_mklink snippets\shellscript.json
+
+  echo [info]: Setup Finished!
+goto End
+:NotAdmin
+  echo This command prompt is NOT ELEVATED
+:End
+
+pause
+
+exit /b
+
+
+REM サブルーチン：バックアップの作成とリンクの作成
+REM call :backup_and_mklink リンク先ファイル リンク元ファイル
+REM 引数が1つだけのときはリンク先とリンク元が同じファイル名とみなす。
+:backup_and_mklink
+if exist "%TO_DIR%\%~1" (
+  MOVE "%TO_DIR%\%~1" "%TO_DIR%\%~1.orig"
 )
-MKLINK "%TO_DIR%\settings.json" "%FROM_DIR%\settings.json"
-
-IF exist "%TO_DIR%\keybindings.json" (
-  MOVE "%TO_DIR%\keybindings.json" "%TO_DIR%\keybindings.json.orig"
+if "%~2"=="" (
+  MKLINK "%TO_DIR%\%~1" "%FROM_DIR%\%~1"
+) else (
+  MKLINK "%TO_DIR%\%~1" "%FROM_DIR%\%~2"
 )
-MKLINK "%TO_DIR%\keybindings.json" "%FROM_DIR%\keybindings.json"
-
-IF exist "%TO_DIR%\snippets\markdown.json" (
-  MOVE "%TO_DIR%\snippets\markdown.json" "%TO_DIR%\snippets\markdown.json.orig"
-)
-MKLINK "%TO_DIR%\snippets\markdown.json" "%FROM_DIR%\snippets\markdown.json"
-
-IF exist "%TO_DIR%\snippets\shellscript.json" (
-  MOVE "%TO_DIR%\snippets\shellscript.json" "%TO_DIR%\snippets\shellscript.json.orig"
-)
-MKLINK "%TO_DIR%\snippets\shellscript.json" "%FROM_DIR%\snippets\shellscript.json"
-
-echo "[info]: Setup Finished!"
-
-PAUSE
+exit /b
