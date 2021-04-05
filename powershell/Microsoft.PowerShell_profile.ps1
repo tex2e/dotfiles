b4951d2a34ac702303cb22146ad759c0fe8e91e5
Mako <mnfeconicu41@gmail.com>
2021-04-05 21:11:22 +0900

    update PowerShell rc

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 
-# --- Title---
+### Title ###
+
 $hostversion="$($Host.Version.Major)`.$($Host.Version.Minor)"
 $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $prp = new-object System.Security.Principal.WindowsPrincipal($wid)
@@ -11,18 +12,28 @@ if ($isAdmin) {
     $Host.UI.RawUI.WindowTitle = "PowerShell $hostversion"
 }
 
-# --- Prompt ---
+
+### Prompt ###
+
 function Prompt {
   Write-Host ("PS " + (Get-Location | Get-Item).Name + ">") -NoNewLine `
     -ForegroundColor Green
   return " "
 }
 
-# --- Alias ---
+
+### Options ###
+
+# Zsh風のタブ補完
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+
+### Alias ###
+
 Set-Alias open Invoke-Item
 
 
-# --- Function ---
+### Function ###
 
 # コマンドの場所やエイリアスの定義を取得する
 function which($cmdname) {
@@ -31,13 +42,11 @@ function which($cmdname) {
 
 # 管理者権限でPowerShellを開く
 function Start-PSElevate() {
-	Start-Process powershell.exe -Verb RunAs
+  Start-Process powershell.exe -Verb RunAs
 }
 
 
-
-# Zsh風のタブ補完
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
+### Others ###
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
