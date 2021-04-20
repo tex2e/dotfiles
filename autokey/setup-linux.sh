f8732c5cc139758222f351dfe4087e32134d47f9
Mako <mnfeconicu41@gmail.com>
2021-04-20 22:22:27 +0900

    create autokey for ubuntu

---
+++ autokey/setup-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+export FROM_DIR="$PWD"
+export TO_DIR="$HOME/.config/autokey/data"
+
+# リンクの作成
+function createSymlink {
+  ln -f -s "$FROM_DIR/$1" "$TO_DIR/${2:-$1}"
+}
+
+createSymlink CapsCtrl
