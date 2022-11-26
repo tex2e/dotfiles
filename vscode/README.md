cec4fe5e6215c76456a15df3abf07072f26fec5a
Mako <mnfeconicu41@gmail.com>
2022-11-26 21:47:40 +0900

    update vscode settings for MacOS

--- vscode/README.md
+++ vscode/README.md
@@ -6,12 +6,6 @@ Visual Studio Code の設定
 
 ### インストール手順
 
-1. Linuxの場合、setup-linux.sh を実行
-2. Windowsの場合、setup-win-admin.cmd を管理者権限で実行
-3. MacOSの場合、以下のコマンドを実行
-
-    ```
-    cd ~/Library/Application\ Support/Code/
-    mv User User.bak
-    ln -s ~/.dotfiles/vscode/ User
-    ```
+1. Linuxの場合、setup-linux.sh を実行 (settings.json)
+2. MacOSの場合、setup-macos.sh を実行 (settings-macos.json)
+3. Windowsの場合、setup-win-admin.cmd を管理者権限で実行
--- vscode/setup-macos.sh
+++ vscode/setup-macos.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 export FROM_DIR="$PWD"
-export TO_DIR="$HOME/.config/Code/User/"
+export TO_DIR="$HOME/Library/Application Support/Code/User"
 
 # バックアップの作成とリンクの作成
 function createSymlink {
@@ -15,13 +15,13 @@ function createSymlink {
 
 case `uname` in
   Darwin ) # mac os
-    # Nothing to do.
-    ;;
-  Linux )
-    createSymlink settings.json
+    createSymlink settings-macos.json settings.json
     createSymlink keybindings.json
     createSymlink snippets/markdown.json
     createSymlink snippets/shellscript.json
+    ;;
+  Linux )
+    # Nothing to do.
     ;;
 esac
 
